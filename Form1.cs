dbdb8fcdfafe59892ac0ce1209b5c57855cba44c
Alejandro Ocampo Cisneros <ocampoalejandro.oc@gmail.com>
2021-01-14 14:01:56 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Mapeo_de_Pixeles
+{
+    public partial class btnMap : Form
+    {
+        public btnMap()
+        {
+            InitializeComponent();
+        }
+    }
+}
